a956705c47b4cd38f95c413f78b4f86d2c25e5c8
iamsajad <s.pr98@yahoo.com>
2024-05-28 08:41:41 +0330

    user controller added

---
+++ controller/user.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once "../helper/middleware.php";
+require_once "../helper/includes.php";
+require_once '../helper/function.php';
+
+use Rakit\Validation\Validator;
+use Carbon\Carbon;
+
+middleware_user_login(request()->data);
+
+$db = new DB();
+
+if(request()->get->method == "get-info"){
+    
+    $validator = new Validator();
+
+    $validation = $validator->make((array) request()->data, 
+    [
+        'user_id' => "required",
+        'token' => "required"
+    ] , 
+    [
+        'user_id:required' => 'آیدی کاربر اجباری می باشد',
+        'token:required' => 'فیلد token اجباری می باشد'
+    ]);
+
+    $validation->validate();
+
+    if($validation->fails()){
+        $data = (object)[
+            'validation_failure' => $validation->fails(),
+            'errors' => $validation->errors()->firstOfAll(),
+            'code' => 400
+        ];
+
+        return response_json($data, $data->code);
+    }
+
+    $user_id = request()->data->user_id;
+    $user = $db->get('users', [], "id = $user_id");
+
+    $response = (object)[
+        "row" => [
+            "user_info" => $user->fetch_object()
+        ],
+        "message" => "اطلاعات دریافت شد",
+        "code" => 200
+    ];
+
+    return response_json($response, $response->code);
+}
